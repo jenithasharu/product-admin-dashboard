57f6901954190b26018c9df3bb4bc502446e626d
jenithasharu <jenithasharu@gmail.com>
2026-09-24 23:48:29 +0530

    Redirect homepage to login

--- app/page.js
+++ app/page.js
@@ -1,20 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import api from "./lib/axios";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default function home(){
-  const [products, setProducts]=useState([])
+export default function Home() {
+  const router = useRouter();
 
-  useEffect(()=> {
-    api.get("/products").then((res)=> {
-      console.log(res.data);
-      setProducts(res.data.products);
-    })
-  },[]);
-  return(
-    <div className="p-8">
-      <h1 className="text-2xl font-bold">Products: {products.length}</h1>
-    </div>
-  )
+  useEffect(() => {
+    router.push("/login");
+  }, []);
+
+  return null;
 }
